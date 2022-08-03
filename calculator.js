07d646420a991dca2cce5407d1b250f6c7d77b62
David <david-williams-1996@outlook.com>
2022-08-03 15:41:12 +1000

    Add basic calculation functions
    Add subtract divide multipy

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add (first, second) {
+    result = first + second;
+    return result;
+}
+
+function subtract (first, second) {
+    result = first - second;
+    return result;
+}
+
+function divide (first, second) {
+    result = first / second;
+    return result;
+}
+
+function multiply (first, second) {
+    result = first * second;
+    return result;
+}
