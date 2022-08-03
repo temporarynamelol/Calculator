e083c9203c5421347fcc80f1e3ae22c545418ae2
David <david-williams-1996@outlook.com>
2022-08-03 15:49:56 +1000

    Add operate function

--- calculator.js
+++ calculator.js
@@ -17,3 +17,18 @@ function multiply (first, second) {
     result = first * second;
     return result;
 }
+
+function operate (first, operator, second) {
+    let result = 0;
+    if (operator == '+') {
+        result = add(first, second);
+    } else if (operator == '-') {
+        result = subtract(first, second);
+    } else if (operator == '/') {
+        result = divide(first, second);
+    } else if (operator == '*') {
+        result = multiply(first, second);
+    }
+
+    return result;
+}
