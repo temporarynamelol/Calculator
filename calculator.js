db81c0b6e3be13568e4fc45fcfef44cdf81fad4f
David <david-williams-1996@outlook.com>
2022-08-04 13:21:21 +1000

    Display text when number clicked

--- calculator.js
+++ calculator.js
@@ -32,3 +32,26 @@ function operate (first, operator, second) {
 
     return result;
 }
+
+const display = document.querySelector('.screenVal');
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let symbol = '';
+        let value = button.innerText.toLowerCase();
+        if (value == 'clear') {
+            display.textContent = '';
+        } else if (value == 'x' || value == '+' || value == '-' || value == '/') {
+            symbol = value;
+            console.log(symbol);
+            display.textContent = '';
+        }
+        else { 
+            //console.log(value);
+            display.textContent += value;
+        }
+
+    })
+})
